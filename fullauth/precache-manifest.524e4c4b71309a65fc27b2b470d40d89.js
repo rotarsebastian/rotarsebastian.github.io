b4a016a98d9ee58c3fab18a89708edd7ebebbddf
Sebastian Rotar <rotar.seby1@gmail.com>
2020-04-27 10:32:29 +0200

    auto-commit

---
+++ fullauth/precache-manifest.524e4c4b71309a65fc27b2b470d40d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce3505a6a25791ca348f5bef5a825aa",
+    "url": "/fullauth/index.html"
+  },
+  {
+    "revision": "872ad3e23b258bbc63a3",
+    "url": "/fullauth/static/css/main.440a4674.chunk.css"
+  },
+  {
+    "revision": "42be927c55bf2e5614ed",
+    "url": "/fullauth/static/js/2.27241935.chunk.js"
+  },
+  {
+    "revision": "334cd8f7728856569f083818dec6000a",
+    "url": "/fullauth/static/js/2.27241935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "872ad3e23b258bbc63a3",
+    "url": "/fullauth/static/js/main.bebf9e34.chunk.js"
+  },
+  {
+    "revision": "ef860f5a7f28b259eaf2",
+    "url": "/fullauth/static/js/runtime-main.5880e812.js"
+  }
+]);
